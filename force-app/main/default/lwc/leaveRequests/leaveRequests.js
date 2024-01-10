02250a065417b834714080614f90bd015dd43617
RSChikkala <ramyashanthisalugu@gmail.com>
2024-01-10 12:35:24 -0500

    initial from mac

---
+++ force-app/main/default/lwc/leaveRequests/leaveRequests.js
@@ -0,0 +1,94 @@
+import { LightningElement ,api,wire} from 'lwc';
+import getLeaveRequests from '@salesforce/apex/LeaveRequestController.getLeaveRequests';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent'
+import Id from '@salesforce/user/Id';
+import {refreshApex} from '@salesforce/apex'
+const COLUMNS = [
+    { label: 'Request Id' , fieldName: 'Name', cellAttributes: { class: { fieldName: 'cellClass' } }},
+    { label: 'User' , fieldName: 'userName', cellAttributes: { class: { fieldName: 'cellClass' } }},
+    { label: 'From Date' , fieldName: 'From_Date__c',cellAttributes: { class: { fieldName: 'cellClass' }}},
+    { label: 'To Date' , fieldName: 'To_Date__c',cellAttributes: { class: { fieldName: 'cellClass' }}}, 
+    { label: 'Reason' , fieldName: 'Reason__c',cellAttributes: { class: { fieldName: 'cellClass' } }},
+    { label: 'Status' , fieldName: 'Status__c',cellAttributes: { class: { fieldName: 'cellClass' } }},
+    { label: 'Manager Comment' , fieldName: 'Manager_Comment__c',cellAttributes: { class: { fieldName: 'cellClass' } }},
+    { type: "button" ,typeAttributes : {
+        label: 'Edit',
+        name: 'Edit',
+        title:'Edit',
+        value:'edit',
+        disabled: { fieldName: 'isEditDisabled' }
+        }, cellAttributes: { class: { fieldName: 'cellClass' } }
+    }
+
+];
+export default class LeaveRequest extends LightningElement {
+
+    columns = COLUMNS;
+    leaveRequests = [];
+    leaveRequestsWireResult;
+    showModalPopUp = false;
+    objectApiName = 'LeaveRequest__c';
+    recordId = '';
+    currentUserId = Id;
+    @wire(getLeaveRequests)
+    wiredLeaveRequests(result)
+    {
+        this.leaveRequestsWireResult = result;
+        if(result.data)
+        {
+            this.leaveRequests = result.data.map(a => ({
+                ...a,
+                userName: a.User__r.Name,
+                cellClass: a.Status__c == 'Approved' ? 'slds-theme_success' : a.Status__c == 'Rejected' ? 'slds-theme_warning' : '',
+                isEditDisabled: a.Status__c != 'Pending'
+            }));
+        }
+        if(result.error)
+        {
+            console.log('Error occured while retrieving the user\'s leaves ', result.error);
+        }
+
+    }
+
+
+    get noRecordsFound()
+    {
+        return (this.leaveRequests.length == 0);
+    }
+    newRequestClickHandler()
+    {
+        this.showModalPopUp = true;
+        this.recordId = '';
+    }
+    popUpCloseHandler()
+    {
+        this.showModalPopUp = false;
+    }
+    rowActionHandler(event)
+    {
+        this.showModalPopUp = true;
+        this.recordId = event.detail.row.Id;
+    }
+    successHandler(event)
+    {
+        this.showModalPopUp=false;
+        this.showToast('Data saved successfully');
+       
+        this.refreshGrid();
+      
+
+    }
+    @api
+    refreshGrid()
+    {
+        refreshApex(this.leaveRequestsWireResult);
+
+    }
+
+    showToast(message,title='success',variant='success'){
+        const event = new ShowToastEvent({
+            title,message,variant
+        });
+        this.dispatchEvent(event);
+    }
+}
